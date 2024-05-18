3fa123a8eac9b807ecced218ff9520ea906ba07c
Mphare1 <thipanemp@gmail.com>
2024-05-18 16:48:06 +0200

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -21,4 +22,28 @@ catch(error) {
     next(error);
 
 }
+};
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if(!email || !password || !email === "" || !password === "") {
+      return  next(errorHandler(400, "All fields are required"));
+    }
+    try {
+        const validuser = await User.findOne({ email });
+        if(!validuser) {
+           return next(errorHandler(404, "User not found"));
+        }
+        const isMatch = bcryptjs.compareSync(password, validuser.password);
+        if(!isMatch) {
+           return next(errorHandler(400, "Invalid credentials"));
+        }
+        const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+        const {password: pass, ...rest} = validuser._doc;
+           res.status(200).cookie('access_token', token, {
+            httpOnly: true})
+            .json(validuser);
+    } catch (error) {
+        next(error);
+    }
+
 };
