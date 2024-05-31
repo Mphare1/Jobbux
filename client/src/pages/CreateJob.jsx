5465d413bc6216d62b1dab0fa949354df4fca968
Mphare1 <thipanemp@gmail.com>
2024-05-31 11:48:10 +0200

    create post

--- client/src/pages/CreateJob.jsx
+++ client/src/pages/CreateJob.jsx
@@ -1,76 +1,108 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react';
-import React, { useState, useEffect } from 'react';
+import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
 import { app } from '../firebase';
+import { useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateJob() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
-  const [imageError, setImageError] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
 
-  const handleUploadImage = async () => {
+  const navigate = useNavigate();
+
+  const handleUpdloadImage = async () => {
     try {
       if (!file) {
-        setImageError('Please select an image to upload');
+        setImageUploadError('Please select an image');
         return;
       }
-      setImageError(null);
+      setImageUploadError(null);
       const storage = getStorage(app);
       const fileName = new Date().getTime() + '-' + file.name;
-      const storageRef = ref(storage, 'jobImages/' + fileName);
+      const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
-
       uploadTask.on(
         'state_changed',
         (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
-          setImageError(error.message);
+          setImageUploadError('Image upload failed');
           setImageUploadProgress(null);
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
             setImageUploadProgress(null);
-            setImageError(null);
-            setFormData((prevData) => ({ ...prevData, jobImage: downloadURL }));
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
           });
         }
       );
     } catch (error) {
-      setImageError('Image upload failed, please try again later');
+      setImageUploadError('Image upload failed');
       setImageUploadProgress(null);
+      console.log(error);
     }
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
 
-  useEffect(() => {
-    if (file) {
-      handleUploadImage();
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
     }
-  }, [file]);
-
+  };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='my-7 text-center font-semibold text-3xl'>Create Job</h1>
-      <form className='flex flex-col space-y-5'>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Create job post</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
-            placeholder='Job Title'
+            placeholder='Title'
             required
-            id='jobTitle'
+            id='title'
             className='flex-1'
-            onChange={(e) => setFormData((prevData) => ({ ...prevData, jobTitle: e.target.value }))}
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
-          <Select id='jobType' onChange={(e) => setFormData((prevData) => ({ ...prevData, jobType: e.target.value }))}
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
           >
-            <option value='uncategorized'>Select Type</option>
+ <option value=''>Select Type</option>
             <option value='fulltime'>Full Time</option>
             <option value='parttime'>Part Time</option>
             <option value='internship'>Internship</option>
@@ -83,22 +115,57 @@ export default function CreateJob() {
             <option value='hybrid'>Hybrid</option>
           </Select>
         </div>
-        <div className='flex gap-4 items-center sm:flex-row justify-between border-4 border-teal-500 border-dotted p-3'>
-          <FileInput type='file' id='jobImage' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
-          <Button gradientDuoTone='purpleToBlue' size='sm' outline onClick={handleUploadImage} disabled={imageUploadProgress !== null}>
-            {imageUploadProgress !== null ? (
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+          <FileInput
+            type='file'
+            accept='image/*'
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <Button
+            type='button'
+            gradientDuoTone='purpleToBlue'
+            size='sm'
+            outline
+            onClick={handleUpdloadImage}
+            disabled={imageUploadProgress}
+          >
+            {imageUploadProgress ? (
               <div className='w-16 h-16'>
-                <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress}%`} />
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                />
               </div>
             ) : (
               'Upload Image'
             )}
           </Button>
-          {imageError && <span className='text-red-500'>{imageError}</span>}
-            {formData.jobImage && <img src={formData.jobImage} alt='Job' className='w-16 h-16 object-cover rounded-md' />}
         </div>
-        <ReactQuill theme='snow' placeholder='Job Description' className='h-72 mb-12' required onChange={(value) => setFormData((prevData) => ({ ...prevData, jobDescription: value }))} />
-        <Button type='submit' gradientDuoTone='purpleToBlue' size='lg'>Publish</Button>
+        {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
+        {formData.image && (
+          <img
+            src={formData.image}
+            alt='upload'
+            className='w-full h-72 object-cover'
+          />
+        )}
+        <ReactQuill
+          theme='snow'
+          placeholder='Write something...'
+          className='h-72 mb-12'
+          required
+          onChange={(value) => {
+            setFormData({ ...formData, content: value });
+          }}
+        />
+        <Button type='submit' gradientDuoTone='purpleToBlue'>
+          Publish
+        </Button>
+        {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
   );
